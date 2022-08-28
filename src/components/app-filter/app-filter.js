9239951d59090579fcfcdf9cc6e6ffbdd82128e4
Nick <Nick_II2000@mail.ru>
2022-08-28 13:40:21 +0600

    32-th commit

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -16,7 +16,7 @@ const AppFilter = () => {
             <button
                 className="btn btn-outline-light"
                 type="button">
-                З/П больше 100 000 руб.
+                З/П больше 100 000 руб. !!!
             </button>
         </div >
     );
