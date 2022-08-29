0d0064fb262f97ebb68b7932613bfcc96664be2d
Nick <Nick_II2000@mail.ru>
2022-08-29 08:41:29 +0600

    40-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,7 +9,7 @@ import './app.css';
 function WhoAmI({ name, surname, link }) {
     return (
         <div>
-            <h1>My name is {name.firstName}, surname - {surname}</h1>
+            <h1>My name is {name}, surname - {surname}</h1>
             <a href={link}>My profile</a>
         </div>
     );
