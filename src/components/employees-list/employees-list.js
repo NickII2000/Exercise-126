a8705e61f5cc2568791cc125777974db00a01450
Nick <Nick_II2000@mail.ru>
2022-08-29 20:29:19 +0600

    49-th commit

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,9 +5,9 @@ import './employees-list.css';
 const Employeeslist = () => {
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem />
-            <EmployeesListItem />
-            <EmployeesListItem />
+            <EmployeesListItem name="John C." salary={80000} />
+            <EmployeesListItem name="Alex M." salary={100000} />
+            <EmployeesListItem name="Carl W." salary={120000} />
         </ul>
     );
 }
