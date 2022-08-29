4f29e158b7357c846873ab9f5e8ca66659c6f747
Nick <Nick_II2000@mail.ru>
2022-08-29 07:50:09 +0600

    34-th commit

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -9,7 +9,6 @@ const Employeeslist = () => {
             <EmployeesListItem />
             <EmployeesListItem />
             <EmployeesListItem />
-            <EmployeesListItem />
         </ul>
     );
 }
