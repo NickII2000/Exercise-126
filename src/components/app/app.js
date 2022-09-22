c9767fb38e03e2b841dc16b2e25873ef88edeb85
Nick <Nick_II2000@mail.ru>
2022-09-22 21:05:05 +0600

    130-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -27,7 +27,7 @@ class App extends Component {
     dec = () => {
         if (this.state.counter > -10) {
             this.setState(state => ({
-                counter: state.counter - 1,
+                counter: state.counter - 2,
             }));
         }
     }
