60236251d9f1094eb0865b554131410224ba7138
Nick <Nick_II2000@mail.ru>
2022-09-26 19:57:57 +0600

    139-th commit

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,10 +1,10 @@
 import './employees-list-item.css';
 
-const EmployeesListItem = () => {
+const EmployeesListItem = (props) => {
     return (
         <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="100 000 руб." />
+            <span className="list-group-item-label">{props.name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={props.salary + ' руб.'} />
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm ">
