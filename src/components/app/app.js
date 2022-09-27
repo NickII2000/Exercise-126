b821a3bd12fedbf4d6287adc2154030021535bc7
Nick <Nick_II2000@mail.ru>
2022-09-27 19:34:24 +0600

    147-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,16 +5,14 @@ import Employeeslist from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
-// import { constants } from 'fs/promises';
 
 function App() {
 
     const data = [
-        { name: 'John C.', salary: 800, increase: false },
-        { name: 'Alex M.', salary: 3000, increase: true },
-        { name: 'Carl W.', salary: 15000, increase: false }
+        { name: "John C.", salary: 80000, increase: false, id: 1 },
+        { name: "Alex M.", salary: 100000, increase: true, id: 2 },
+        { name: "Carl W.", salary: 120000, increase: false, id: 3 }
     ];
-
     return (
         <div className="app">
             <AppInfo />
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -5,10 +5,17 @@ import './employees-list.css';
 const Employeeslist = ({ data }) => {
 
     const elements = data.map(item => {
+        const { id, ...itemProps } = item;
+
+        console.log(id);
+        console.log(itemProps);
+
         return (
-            <EmployeesListItem {...item} />
+            <EmployeesListItem key={id} {...itemProps} />
         );
     });
+
+    console.log(elements);
 
     return (
         <ul className="app-list list-group">
