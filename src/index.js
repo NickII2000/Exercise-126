857a2588d553b614b32a9f74dc5ff688a9c085b2
Nick <Nick_II2000@mail.ru>
2022-10-01 12:47:29 +0600

    164-th commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,8 @@ import App from './components/app/app';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App counter={10} />
-    <App counter={-10} />
+    <App counter={0} />
+    <App counter={50} />
+    <App counter={-50} />
   </React.StrictMode>
 );
