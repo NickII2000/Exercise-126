c617b8a7f6818b9e570e7d3bec03e46e951a1e60
Nick <Nick_II2000@mail.ru>
2022-10-03 21:05:57 +0600

    190-th commit

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -18,6 +18,8 @@ class EmployeesAddForm extends Component {
     }
 
     render() {
+        const { name, salary } = this.state;
+
         return (
             <div className="app-add-form" >
                 <h3>Добавьте нового сотрудника</h3>
@@ -27,11 +29,13 @@ class EmployeesAddForm extends Component {
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
                         name="name"
+                        value={name}
                         onChange={this.onValueChange} />
                     <input type="number"
                         className="form-control new-post-label"
                         placeholder="З/П в руб.?"
                         name="salary"
+                        value={salary}
                         onChange={this.onValueChange} />
 
                     <button type="submit"
