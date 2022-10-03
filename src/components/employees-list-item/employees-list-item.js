ba0c4fc2accef36d0f758aa98c016f9660a0d161
Nick <Nick_II2000@mail.ru>
2022-10-03 08:19:34 +0600

    188-th commit

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -6,7 +6,8 @@ class EmployeesListItem extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            increase: false
+            increase: false,
+            like: false,
         };
     }
 
@@ -16,18 +17,27 @@ class EmployeesListItem extends Component {
         }));
     }
 
+    onLike = () => {
+        this.setState(({ like }) => ({
+            like: !like
+        }));
+    }
+
     render() {
         const { name, salary } = this.props;
-        const { increase } = this.state;
+        const { increase, like } = this.state;
 
         let className = 'list-group-item d-flex justify-content-between';
         if (increase) {
             className += ' increase';
         }
+        if (like) {
+            className += ' like';
+        }
 
         return (
             <li className={className}>
-                <span className="list-group-item-label">{name}</span>
+                <span className="list-group-item-label" onClick={this.onLike}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + ' руб.'} />
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
