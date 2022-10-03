7d3f3fdc1123a0a7b7085a18b1fc9fd6ebdb57bc
Nick <Nick_II2000@mail.ru>
2022-10-03 07:33:33 +0600

    185-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,9 +4,9 @@ import AppFilter from '../app-filter/app-filter';
 import Employeeslist from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-import './app.css';
-
 import { Component } from 'react';
+
+import './app.css';
 
 // class WhoAmI extends Component {
 //     constructor(props) {
