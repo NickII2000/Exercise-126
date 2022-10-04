5563db124ffe72c9af24e617a9eb3dd9da317101
Nick <Nick_II2000@mail.ru>
2022-10-04 21:35:19 +0600

    209-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -130,18 +130,27 @@ class App extends Component {
 
     onToggleIncrease = (id) => {
         // console.log(`Increase this ${id}`);
-        this.setState(({ data }) => {
-            const index = data.findIndex(elem => elem.id === id);
+        // this.setState(({ data }) => {
+        // const index = data.findIndex(elem => elem.id === id);
 
-            const old = data[index];
-            const newItem = { ...old, increase: !old.increase };
-            const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
+        // const old = data[index];
+        // const newItem = { ...old, increase: !old.increase };
+        // const newArr = [...data.slice(0, index), newItem, ...data.slice(index + 1)];
 
-            return {
-                data: newArr
-            };
-        });
+        // return {
+        //     data: newArr
+        // };
 
+        // });
+        this.setState(({ data }) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return { ...item, increase: !item.increase }
+                } //else {
+                return item;
+                //}
+            })
+        }));
     }
 
     onToggleRise = (id) => {
