055f8f3a83b4379c585d781854f1d83b898b5139
Nick <Nick_II2000@mail.ru>
2022-10-10 08:46:17 +0600

    257-th commit

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -16,7 +16,9 @@ const EmployeesListItem = (props) => {
         <li className={className}>
             <span className="list-group-item-label"
                 onClick={onToggleProp}
-                data-toggle="rise">{name}</span>
+                data-toggle="rise"
+            // style={{ fontSize: '40px', color: 'red', transition: 'all', WebkitTransition: 'all', msTransition: 'all' }}
+            >{name}</span>
             <input type="text" className="list-group-item-input" defaultValue={salary + ' руб.'} />
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
