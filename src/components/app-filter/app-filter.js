cf3f0a31ba9f1a75ed74e5d40057eba19cd7072c
Nick <Nick_II2000@mail.ru>
2022-10-11 07:31:23 +0600

    267-th commit

--- src/components/app-filter/app-filter.js
+++ src/components/app-filter/app-filter.js
@@ -2,13 +2,13 @@ import './app-filter.css';
 
 const AppFilter = (props) => {
     const buttonsData = [
-        { name: 'all', label: 'Все сотрудники' },
-        { name: 'rise', label: 'На повышение' },
-        { name: 'increase', label: 'Премированные' },
-        { name: 'from100000', label: 'Зарплата от 100 000 руб. и выше' }
+        { name: 'all', label: 'Все сотрудники', colored: false },
+        { name: 'rise', label: 'На повышение', colored: false },
+        { name: 'increase', label: 'Премированные', colored: false },
+        { name: 'from100000', label: 'Зарплата от 100 000 руб. и выше', colored: true }
     ]
 
-    const buttons = buttonsData.map(({ name, label }) => {
+    const buttons = buttonsData.map(({ name, label, colored }) => {
         const active = props.filter === name;
         const clazz = active ? 'btn-light' : 'btn-outline-light';
         return (
@@ -16,9 +16,10 @@ const AppFilter = (props) => {
                 type="button"
                 className={`btn ${clazz}`}
                 key={name}
-                onClick={() => props.onFilterSelect(name)}>
+                onClick={() => props.onFilterSelect(name)}
+                style={colored ? { color: 'sea' } : null}>
                 {label}
-            </button>
+            </button >
         )
     });
 
