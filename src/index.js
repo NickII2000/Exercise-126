f6f8cf47b9ff622dbfb3fc8f031a7265941a297f
Nick <Nick_II2000@mail.ru>
2022-10-11 15:06:56 +0600

    280-th commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 import { Button } from './components/app/app';
 
 import './index.scss';
-const BigButton = styled('Button')`
+const BigButton = styled(Button)`
   margin: 0 auto;
   width: 245px;
 `;
