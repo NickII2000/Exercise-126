73338e502be625a6f7aed7e0b9f9cd50372ef360
Nick <Nick_II2000@mail.ru>
2022-10-11 15:01:07 +0600

    279-th commit

--- src/index.js
+++ src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+import styled from 'styled-components';
 import { Button } from './components/app/app';
 
 import './index.scss';
+const BigButton = styled('Button')`
+  margin: 0 auto;
+  width: 245px;
+`;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Button>Дополнительная кнопка</Button>
+    <BigButton>Большая кнопка</BigButton>
   </React.StrictMode>
 );
