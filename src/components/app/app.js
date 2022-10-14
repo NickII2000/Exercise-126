5ba4f1381e409f1181e0cee9484e671c7699dcaf
Nick <Nick_II2000@mail.ru>
2022-10-14 22:39:06 +0600

    292-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -66,7 +66,7 @@ class WhoAmI extends Component {
         const { name, surname, link } = this.props;
         const { years, text, position } = this.state;
 
-        console.log(this);
+        // console.log(this);
 
         return (
             <EmpItem active>
