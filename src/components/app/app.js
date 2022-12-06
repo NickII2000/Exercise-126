1100c09e20a3ae71de4027cb7559b7104ea019a4
Nick <Nick_II2000@mail.ru>
2022-12-06 17:35:00 +0600

    362-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -307,10 +307,20 @@ const DynamicGreating = (props) => {
 
 }
 
+const HelloGreating = () => {
+    return (
+        <div style={{ 'width': '600px', 'margin': '0 auto' }}>
+            <DynamicGreating color={'primary'}>
+                <h2>Hello world!</h2>
+            </DynamicGreating>
+        </div>
+    )
+}
+
 function App() {
     return (
         <Wrapper>
-
+            <HelloGreating />
             <BootstrapTest
                 left={
                     <DynamicGreating color={'primary'}>
