98ab202135fbfb6bcbe0b3cd48528b20ad959775
Nick <Nick_II2000@mail.ru>
2022-12-15 13:23:54 +0600

    382-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -21,21 +21,26 @@ class Form extends Component {
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
-                    <div
-                        style={{
-                            'width': '500px',
-                            'height': '150px',
-                            'backgroundColor': 'red',
-                            'position': 'absolute',
-                            'right': '-50%',
-                            'bottom': '-50%'
-                        }}>
-                        Hello
-                    </div>
                 </form>
             </Container>
         )
     }
+}
+
+const Msg = () => {
+    return (
+        <div
+            style={{
+                'width': '500px',
+                'height': '150px',
+                'backgroundColor': 'red',
+                'position': 'absolute',
+                'right': '-50%',
+                'bottom': '-50%'
+            }}>
+            Hello
+        </div>
+    )
 }
 
 function App() {
