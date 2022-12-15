e891875fe33889f06f2b27f37a20e7672e950303
Nick <Nick_II2000@mail.ru>
2022-12-15 12:32:59 +0600

    377-th commit

--- src/index.js
+++ src/index.js
@@ -38,16 +38,9 @@ import styled from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// const BigButton = styled(Button)`
-//     margin: 0 auto;
-//     width: 245px;
-//     text-align: center;
-// `;
-
 ReactDOM.render(
   <StrictMode>
     <App />
-    {/* <BigButton as="a">Отправить отчет</BigButton> */}
   </StrictMode>,
   document.getElementById('root')
 );
