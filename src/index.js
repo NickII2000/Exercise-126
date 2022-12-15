c281a5240f248c4fb0b7513152945542e9b2d983
Nick <Nick_II2000@mail.ru>
2022-12-15 12:48:12 +0600

    380-th commit

--- src/index.js
+++ src/index.js
@@ -19,8 +19,6 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
-// import reportWebVitals from './reportWebVitals';
-// import styled from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
