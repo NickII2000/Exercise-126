b58c9aa6aad2dc1837e612b8f303609963ef1ddd
Nick <Nick_II2000@mail.ru>
2022-12-15 12:47:29 +0600

    379-th commit

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
+// import React, { StrictMode } from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './components/app/app';
+// import styled from 'styled-components';
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+// ReactDOM.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   document.getElementById('root')
+// );
+
+
+
 import React, { StrictMode } from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/app/app';
-import styled from 'styled-components';
+// import reportWebVitals from './reportWebVitals';
+// import styled from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById('root')
+  </StrictMode>
 );
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
