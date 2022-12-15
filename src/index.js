3139d44e9801f8975731b6e7dea6bff3b5636c2c
Nick <Nick_II2000@mail.ru>
2022-12-15 12:39:22 +0600

    378-th commit

--- src/index.js
+++ src/index.js
@@ -1,39 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import App from './components/app/app';
-
-// // import styled from 'styled-components';
-// // import { Button } from './components/app/app';
-
-// // import BootstrapTest from './BootstrapTest';
-
-// import './index.scss';
-
-// // import 'bootstrap/dist/css/bootstrap.min.css';
-
-// // const BigButton = styled(Button)`
-// //   margin: 0 auto;
-// //   width: 545px;
-// //   text-align: center;
-// // `;
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//     {/* <Button>Дополнительная кнопка</Button>
-//     <BigButton>Большая кнопка</BigButton>
-//     <BigButton as="a">Ссылка тег a</BigButton>
-//     <BootstrapTest /> */}
-//   </React.StrictMode >
-// );
-
-
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/app';
-// import { Button } from './components/app/app';
 import styled from 'styled-components';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -44,3 +12,18 @@ ReactDOM.render(
   </StrictMode>,
   document.getElementById('root')
 );
+
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
