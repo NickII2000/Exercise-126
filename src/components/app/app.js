3dcaa5056421083f6bdd21ae76ac4ada273ac68a
Nick <Nick_II2000@mail.ru>
2022-12-15 13:24:40 +0600

    383-th commit

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -21,6 +21,7 @@ class Form extends Component {
                         <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
                         <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                     </div>
+
                 </form>
             </Container>
         )
